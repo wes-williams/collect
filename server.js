ace5893fbd5f04ad4a25313bd4d1f7c1835063a8
Wes Williams <wwwestech@gmail.com>
2013-08-16 00:16:14 -0600

    added support for all methods on routes

--- server.js
+++ server.js
@@ -150,7 +150,7 @@ var SampleApp = function() {
 
         //  Add handlers for the app (from the routes).
         for (var r in self.routes) {
-            self.app.get(r, self.routes[r]);
+            self.app.all(r, self.routes[r]);
         }
     };
 
