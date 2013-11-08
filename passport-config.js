1f001099780805bd2d0a2797ecc96cf242f1403d
Wes Williams <wwwestech@gmail.com>
2013-11-07 18:41:26 -0700

    back to running

--- passport-config.js
+++ passport-config.js
@@ -9,7 +9,7 @@ appConfig.fitbit.accessTokenUrl = 'http://www.fitbit.com/oauth/access_token';
 appConfig.fitbit.defaultLoadUrl = 'https://api.fitbit.com/1/user/-/profile.json'; 
 appConfig.fitbit.userIdPath = 'user.encodedId'; 
 appConfig.fitbit.clientId = '3f9ee2e269be43d397cd98acf7c53853';
-appConfig.fitbit.clientSecret = '58d8fd9bb9ed491c9ca7a26681141ba6';
+appConfig.fitbit.clientSecret = '58d8fd9bb9ed491c9ca7a26681141ba6'; 
 //appConfig.fitbit.callbackUrl = 'http://127.0.0.1:8080/auth/fitbit/callback';
 appConfig.fitbit.callbackUrl = 'https://proxy-dataupco.rhcloud.com/auth/fitbit/callback';
 
