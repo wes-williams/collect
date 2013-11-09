1c81eb3c464317378c7458f8ecb310a94d5b3ebc
Wes Williams <wwwestech@gmail.com>
2013-11-09 00:10:55 -0700

    removed expired credentials, cleaned up config

--- passport-config.js
+++ passport-config.js
@@ -4,6 +4,7 @@ var appConfig = {};
 // oauth 1.0
 ///////////////
 
+// fitbit - https://wiki.fitbit.com/display/API/Fitbit+API
 
 appConfig.fitbit = {};
 appConfig.fitbit.type='oauth-1.0';
@@ -13,9 +14,11 @@ appConfig.fitbit.accessTokenUrl = 'http://www.fitbit.com/oauth/access_token';
 appConfig.fitbit.baseUrl = 'https://api.fitbit.com/1'; 
 appConfig.fitbit.defaultLoadUrl = '/user/-/profile.json'; 
 appConfig.fitbit.userIdPath = 'user.encodedId'; 
-appConfig.fitbit.clientId = '1cf532fb0592432a9f66debe91704023';
-appConfig.fitbit.clientSecret = '9e8964f700a84755a9d1522c1adcab52'; 
+appConfig.fitbit.clientId = '';
+appConfig.fitbit.clientSecret = '';
 appConfig.fitbit.callbackUrl = 'https://proxy-dataupco.rhcloud.com/auth/fitbit/callback';
+
+// twitter - https://dev.twitter.com/docs/api/1.1
 
 appConfig.twitter = {};
 appConfig.twitter.type='oauth-1.0';
@@ -25,20 +28,28 @@ appConfig.twitter.accessTokenUrl = 'https://api.twitter.com/oauth/access_token';
 appConfig.twitter.baseUrl = 'http://api.twitter.com/1.1'; 
 appConfig.twitter.defaultLoadUrl = '/account/verify_credentials.json'; 
 appConfig.twitter.userIdPath = 'screen_name'; 
-appConfig.twitter.clientId = 'l2toSn0ttK9H8unn2biPww';
-appConfig.twitter.clientSecret = '0jpWODJe2vEG27TXKZMLFweNFXzw8Iq6ZXxFpx6Y'; 
+appConfig.twitter.clientId = '';
+appConfig.twitter.clientSecret = '';
 appConfig.twitter.callbackUrl = 'https://proxy-dataupco.rhcloud.com/auth/twitter/callback';
 
+///////////////
+// oauth 2.0  (no request url)
+///////////////
+
 /*
-// oauth 2.0 (no request url)
-appConfig.fitbit = {};
-appConfig.fitbit.type='oauth-2.0';
-appConfig.fitbit.authorizationUrl = 'http://www.fitbit.com/oauth/authorize';
-appConfig.fitbit.accessTokenUrl = 'http://www.fitbit.com/oauth/access_token';
-appConfig.fitbit.defaultLoadUrl = 'https://api.fitbit.com/1/user/-/profile.json'; 
-appConfig.fitbit.clientId = '';
-appConfig.fitbit.clientSecret = '';
-appConfig.fitbit.callbackUrl = 'https://proxy-dataupco.rhcloud.com/fitbit/callback';
+// github - http://developer.github.com/
+
+appConfig.github = {};
+appConfig.github.type='oauth-2.0';
+appConfig.github.authorizationUrl = 'https://github.com/login/oauth/authorize';
+appConfig.github.accessTokenUrl = 'https://github.com/login/oauth/access_token';
+appConfig.github.baseUrl = 'https://api.github.com'; 
+appConfig.github.defaultLoadUrl = '/user'; 
+appConfig.github.userIdPath = 'login'; 
+appConfig.github.clientId = '';
+appConfig.github.clientSecret = '';
+appConfig.github.callbackUrl = 'https://proxy-dataupco.rhcloud.com/github/callback';
+
 */
 
 module.exports = appConfig;
