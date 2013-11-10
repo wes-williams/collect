2039788130bd49bef42f1dff385631b8ddb7d1c0
Wes Williams <wwwestech@gmail.com>
2013-11-09 22:59:06 -0700

    added placeholder values for key/secret

--- passport-config.js
+++ passport-config.js
@@ -14,8 +14,8 @@ appConfig.fitbit.accessTokenUrl = 'https://www.fitbit.com/oauth/access_token';
 appConfig.fitbit.baseUrl = 'https://api.fitbit.com/1'; 
 appConfig.fitbit.defaultLoadUrl = '/user/-/profile.json'; 
 appConfig.fitbit.userIdPath = 'user.encodedId'; 
-appConfig.fitbit.clientId = '';
-appConfig.fitbit.clientSecret = '';
+appConfig.fitbit.clientId = 'KEY_GOES_HERE';
+appConfig.fitbit.clientSecret = 'PASS_GOES_HERE';
 appConfig.fitbit.callbackUrl = 'https://proxy-dataupco.rhcloud.com/auth/fitbit/callback';
 
 // twitter - https://dev.twitter.com/docs/api/1.1
@@ -28,8 +28,8 @@ appConfig.twitter.accessTokenUrl = 'https://api.twitter.com/oauth/access_token';
 appConfig.twitter.baseUrl = 'http://api.twitter.com/1.1'; 
 appConfig.twitter.defaultLoadUrl = '/account/verify_credentials.json'; 
 appConfig.twitter.userIdPath = 'screen_name'; 
-appConfig.twitter.clientId = '';
-appConfig.twitter.clientSecret = '';
+appConfig.twitter.clientId = 'KEY_GOES_HERE';
+appConfig.twitter.clientSecret = 'PASS_GOES_HERE';
 appConfig.twitter.callbackUrl = 'https://proxy-dataupco.rhcloud.com/auth/twitter/callback';
 
 ///////////////
@@ -45,8 +45,8 @@ appConfig.github.accessTokenUrl = 'https://github.com/login/oauth/access_token';
 appConfig.github.baseUrl = 'https://api.github.com'; 
 appConfig.github.defaultLoadUrl = '/user'; 
 appConfig.github.userIdPath = 'login'; 
-appConfig.github.clientId = '';
-appConfig.github.clientSecret = '';
+appConfig.github.clientId = 'KEY_GOES_HERE';
+appConfig.github.clientSecret = 'PASS_GOES_HERE';
 appConfig.github.callbackUrl = 'https://proxy-dataupco.rhcloud.com/auth/github/callback';
 
 module.exports = appConfig;
