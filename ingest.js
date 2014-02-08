a89ef3a2fcff9fa9fe8b44bc300c963d4a7934a6
Wes Williams <wwwestech@gmail.com>
2014-02-08 13:01:53 -0700

    added case insensitive regex support, and rejected request on invalid params

--- ingest.js
+++ ingest.js
@@ -48,23 +48,28 @@ var query = function(meta,query,done) {
 
   var data = {};
 
-  for(var key in query) {
-    var value = query[key];
-    var operator = 'eq'; // equal is the default operator
+  // inspect and translate the params for query
+  try {
+    for(var key in query) {
+      var value = query[key];
+      var operator = 'eq'; // equal is the default operator
 
-    // key~op indicates which operator should be used.
-    if(key.match(/^.+\~(eq|lt|lte|gt|ne|in|nin|re)$/i)) { // NOTE: case insensitive here
-      var delimPosition = key.lastIndexOf('~');
-      operator = key.substring(delimPosition+1).toLowerCase();
-      key = key.substring(0,delimPosition);
-    }
+      // key~op indicates which operator should be used.
+      if(key.match(/^.+\~(eq|lt|lte|gt|ne|in|nin|re|rei)$/i)) { // NOTE: case insensitive here
+        var delimPosition = key.lastIndexOf('~');
+        operator = key.substring(delimPosition+1).toLowerCase();
+        key = key.substring(0,delimPosition);
+      }
 
-    // make sure the data is clean
-    if(typeof key === "string"            // key is string
-       && typeof value  === "string"      // value is string
-       && key.trim().length > 0           // key must not be empty
-       && key.match(/^[a-zA-Z0-9._-]+$/)   // key only alpha, numeric, dot, underscore, dash
-       && value.trim().length > 0) {      // value must not be empty
+      // make sure the data is clean
+      if(!(typeof key === "string"            // key is string
+         && typeof value  === "string"      // value is string
+         && key.trim().length > 0           // key must not be empty
+         && key.match(/^[a-zA-Z0-9._-]+$/)   // key only alpha, numeric, dot, underscore, dash
+         && value.trim().length > 0)) {      // value must not be empty
+        throw ('invalid parameter: ' + key + ' = ' + value);
+
+      }
 
       // cast values. take into account that value may need to be split
       var castValues = operator.match(/^(in|nin)$/) ? value.split('~') : [value];
@@ -86,14 +91,19 @@ var query = function(meta,query,done) {
       if(operator === "eq") {
         data[key] = value;
       }
-      if(operator === "re") {
+      if(operator === "re" || operator === "rei") {
         try {
-          var regex = new RegExp(value);
-          regex.test('abc');
+          var regex;
+          if(operator === "rei") {
+            regex = new RegExp(value,"i");
+          }
+          else {
+            regex = new RegExp(value);
+          }
           data[key] = regex;
         }
         catch(e) {
-          console.log('invalid regex in query: ' + value);
+          throw ('invalid regex: ' + value);
         }
       }
       else {
@@ -116,9 +126,13 @@ var query = function(meta,query,done) {
         // only doing AND conditions right now
         // in and not in exist for OR conditions
         if(data[key]) {
-          // only use condition if an equal condition not present
+          // only use condition if an equal or regex condition not present
           // equal conditions will be string values
-          if(typeof(data[key]) !== "string") {
+          if(typeof(data[key]) === "string" 
+             || data[key] instanceof Regexp) {
+            throw  'not handling compound conditions for eq, re, or rei operators';
+          }
+          else {
             data[key][conditionKey] = conditionValue;
           }
         }
@@ -127,26 +141,31 @@ var query = function(meta,query,done) {
           data[key][conditionKey] = conditionValue;
         }
       }
-      console.log("QUERY = " + JSON.stringify(data));
     }
+    console.log("QUERY = " + JSON.stringify(data));
+  }
+  catch(ex) {
+    console.log('INVALID QUERY');
+    done(undefined);
+    return;
   }
 
-  // this must be last
-  data['_meta.user'] = meta.user;
-  
-  var options = {
-    "limit": 20
-  };
-                   
-  db.collection('temporary').find(data,options).toArray(function(err,docs){
-     if(err) {
-       console.log('Failed to find matching data: ' + JSON.stringify(err));
-       done(undefined);
-     } 
-     else {
-       done(docs);
-     }
-  });
+// this must be last
+data['_meta.user'] = meta.user;
+
+var options = {
+  "limit": 20
+};
+                 
+db.collection('temporary').find(data,options).toArray(function(err,docs){
+   if(err) {
+     console.log('Failed to find matching data: ' + JSON.stringify(err));
+     done(undefined);
+   } 
+   else {
+     done(docs);
+   }
+});
 
 };
 ingestPlugin.query = query;
