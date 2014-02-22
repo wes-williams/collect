1c7959e19e060bb55b2ff0d6f085b9e14a150000
Wes Williams <wwwestech@gmail.com>
2014-02-22 09:27:33 -0700

    correct auth ui

--- public/js/authSetup.js
+++ public/js/authSetup.js
@@ -1,27 +1,27 @@
 function authSetup() {
-   var authArea = document.getElementById('auth');
-   $.ajax({ 
-      type: 'GET',
-      url: '/auth', 
-      success: function(res, status, xhr) { 
-        var html='';
-        if(Array.isArray(res)) {
-          for(var i=0;i<res.length;i++) {
-            var apiName = res[i].api;
-            var method = res[i].enabled ? 'delete' : 'post';
-            var toggle = res[i].enabled ? 'disable' : 'enable';
-            html += '<span class="auth-switch">'
-            html += '<form name="'+apiName+'" method="'+method+'" action="/auth/'+apiName+'">';
-            html += '<input type="button" value="'+toggle+' '+apiName+'" onclick="'+toggle+'Auth(this.form)" />';
-            html += '</form></span>';
-          }
+  var authArea = document.getElementById('auth');
+  $.ajax({ 
+    type: 'GET',
+    url: '/auth', 
+    success: function(res, status, xhr) { 
+      var html='';
+      if(Array.isArray(res)) {
+        for(var i=0;i<res.length;i++) {
+          var apiName = res[i].api;
+          var method = res[i].enabled ? 'delete' : 'post';
+          var toggle = res[i].enabled ? 'disable' : 'enable';
+          html += '<span class="auth-switch">'
+          html += '<form name="'+apiName+'" method="'+method+'" action="/auth/'+apiName+'">';
+          html += '<input type="button" value="'+toggle+' '+apiName+'" onclick="'+toggle+'Auth(this.form)" />';
+          html += '</form></span>';
         }
-        authArea.innerHTML = html; 
-      },
-      error: function(xhr, status, err) {
-        authArea.innerHTML = ''; 
-       }
-   });
+      }
+      authArea.innerHTML = html; 
+    },
+    error: function(xhr, status, err) {
+      authArea.innerHTML = ''; 
+    }
+  });
 }
 
 function authTeardown() {
