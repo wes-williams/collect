33132ff7a92ebebdb907d161eb8d6715ce9d81fe
Wes Williams <wwwestech@gmail.com>
2014-02-22 09:03:09 -0700

    correct auth ui

---
+++ public/js/authSetup.js
@@ -0,0 +1,42 @@
+function authSetup() {
+   var authArea = document.getElementById('auth');
+   $.ajax({ 
+      type: 'GET',
+      url: '/auth', 
+      success: function(res, status, xhr) { 
+        var html;
+        if(Array.isArray(res)) {
+          for(var i=0;i<res.length;i++) {
+            var apiName = res[i].api;
+            var method = res[i].enabled ? 'delete' : 'post';
+            var toggle = res[i].enabled ? 'disable' : 'enable';
+            html += '<span class="auth-switch">'
+            html += '<form name="'+apiName+'" method="'+method+'" action="/auth/'+apiName+'">';
+            html += '<input type="button" value=""+toggle+' '+apiName'" ';
+            html += 'onclick="'+toggle+'Auth(this.form)" />';
+            html += '</form></span>';
+          }
+        }
+        authArea.innerHTML = html; 
+      },
+      error: function(xhr, status, err) {
+        authArea.innerHTML = ''; 
+       }
+   });
+}
+
+function enableAuth(form) {
+  form.submit();
+}
+
+function disableAuth(form) {
+  $.ajax({ 
+    type: 'GET',
+    url: , 
+    success: function(res, status, xhr) { 
+      authSetup();  
+    },
+    error: function(xhr, status, err) {
+    }
+  });
+}
