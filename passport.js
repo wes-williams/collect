407b06a80dd3d7d6cd7416c93677c8104bc5c92a
Wes Williams <wwwestech@gmail.com>
2014-02-22 13:46:01 -0700

    made sort of allApiDetails case insensitive

--- passport.js
+++ passport.js
@@ -85,6 +85,8 @@ var allApiDetails = function() {
     }
   }
   apis.sort(function(a,b) {
+    a = a.toLowerCase();
+    b = b.toLowerCase();
     if(a.id==b.id) return 0;
     if(a.id<b.id) return -1;
     else return 1;
