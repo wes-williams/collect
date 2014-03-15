14ca986f38759481f0940a7024fa69e7a84b6115
Wes Williams <wwwestech@gmail.com>
2014-03-15 11:22:32 -0600

    generate shorter webhook login

--- server.js
+++ server.js
@@ -450,8 +450,8 @@ var SampleApp = function() {
             return;
           }
 
-          var username = crypto.randomBytes(48).toString('hex');
-          var password = crypto.randomBytes(48).toString('hex');
+          var username = crypto.randomBytes(16).toString('hex');
+          var password = crypto.randomBytes(16).toString('hex');
           var timestamp = new Date().getTime();
 
           var hook = {
